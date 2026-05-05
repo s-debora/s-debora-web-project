578ad0d2629d740497462209badf21efe3fb8f96
Samuel <sdebora@nmsu.edu>
2026-05-04 22:29:29 -0600

    Added Project 4 HTML page

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("worldcupform");
+const message = document.getElementById("message");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("nation").value;
+    const name = document.getElementById("worldcups").value;
+
+    message.textContent = `$(nation), great! "$(worldcups)", wow! That many?`;
+});
