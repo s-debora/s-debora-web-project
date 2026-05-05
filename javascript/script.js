0e4a09aabc568fbc9ea7f9fd6526310d2cc3c291
Samuel <sdebora@nmsu.edu>
2026-05-04 22:47:03 -0600

    Added Project 4 HTML page

--- javascript/script.js
+++ javascript/script.js
@@ -4,8 +4,8 @@ const message = document.getElementById("message");
 form.addEventListener("submit", function(event) {
     event.preventDefault();
 
-    const name = document.getElementById("nation").value;
-    const name = document.getElementById("worldcups").value;
+    const nation = document.getElementById("nation").value;
+    const worldcups = document.getElementById("worldcups").value;
 
-    message.textContent = "$(nation), great! $(worldcups), wow! That many?";
+    message.textContent = `$(nation), great! "$(worldcups)", wow! That many?`;
 });
